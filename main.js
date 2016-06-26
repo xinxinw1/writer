0e15b8b02ada94f66d579fd95b81ebe82d39e506
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2016-06-26 03:49:29 -0400

    update css, no fade in when referred from file.html

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-    fadeIn($('#subtitle1'), 1000);
-    fadeIn($('#subtitle2'), 1000, 300);
-    fadeIn($('#buttons'), 1000, 600);
-    fadeIn($('#files'), 1000, 600);
+    console.log(document.referrer);
+    if (!/file\.html/.test(document.referrer)){
+      fadeIn($('#subtitle1'), 1000);
+      fadeIn($('#subtitle2'), 1000, 300);
+      fadeIn($('#buttons'), 1000, 600);
+      fadeIn($('#files'), 1000, 600);
+    }
 });
 
 function fadeIn(elem, time, delay){
   if (delay === undefined)delay = 0;
-  elem.css({opacity: 0, visibility: "visible"}).delay(delay).animate({opacity: 1}, time);
+  elem.css({opacity: 0}).delay(delay).animate({opacity: 1}, time);
 }
